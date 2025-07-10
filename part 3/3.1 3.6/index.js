47bc160eea54de16b7b02f61cc0cf5ac283a4c2d
nephro-dmi <ai.4.nephro.dmi@enis.tn>
2025-07-10 22:38:44 +0100

    changes

--- part 3/3.1 3.6/index.js
+++ part 3/3.1 3.6/index.js
@@ -1,31 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
 const cors = require("cors");
-
+const Person = require("./models/person");
 app.use(cors());
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+
 //custom token
 morgan.token("detail", (request, response) => {
   if (request.method === "POST") {
@@ -47,7 +27,9 @@ app.use(
 //API Calls
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((p) => {
+    response.json(p);
+  });
 });
 app.get("/info/", (request, response) => {
   const info = persons.length;
@@ -84,24 +66,23 @@ app.post("/api/persons", (request, response) => {
     return response.status(400).json({
       error: "missing number or name",
     });
-  } else if (
+  } /*else if (
     persons.find((p) => p.name.toLowerCase() === body.name.toLowerCase())
   ) {
     return response.status(400).json({
       error: "name must be unique",
     });
-  } else {
-    const person = {
-      id: generateid(),
+  } */ else {
+    const person = new Person({
       name: body.name,
       number: body.number,
-    };
-    persons = persons.concat(person);
-    response.json(persons);
+    });
+    person.save().then((p) => response.json(p));
   }
 });
 //listener
-const port = process.env.port || 3001;
+
+const PORT = process.env.PORT;
 app.listen(3001, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${PORT}`);
 });
